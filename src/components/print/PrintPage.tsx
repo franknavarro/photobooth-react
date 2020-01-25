f6c2988bc5d3ee7b8e713571aa0010c621e29af8
Frank <fnavarro7777@gmail.com>
2020-01-24 18:28:20 -0800

    Added progress bar and print done to print page

--- src/components/print/PrintPage.tsx
+++ src/components/print/PrintPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteChildrenProps } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -6,24 +6,44 @@ import 'components/print/PrintPage.css';
 
 import { StoreState } from 'reducers';
 import { resetPrint, updatePrint } from 'actions';
+import ProgressBar from 'components/ProgressBar';
 
 const PrintPage: React.FC<RouteChildrenProps> = ({ history }) => {
   const { printStatus, printerInCheck } = useSelector(
     (state: StoreState) => state.photostrips,
   );
+  const [showComplete, setShowComplete] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log(printerInCheck);
     if (printStatus >= 0 && printStatus < 100 && !printerInCheck) {
       const updateTimout = setTimeout(() => dispatch(updatePrint()), 1000);
       return () => clearInterval(updateTimout);
-    } else if (printStatus >= 100) {
-      dispatch(resetPrint());
-      history.push('/');
+    } else if (printStatus >= 100 && !showComplete) {
+      setShowComplete(true);
+    } else if (printStatus >= 100 && showComplete) {
+      setTimeout(() => {
+        dispatch(resetPrint());
+        history.push('/');
+      }, 5000);
     }
-  }, [printStatus, printerInCheck, history, dispatch]);
+  }, [printStatus, printerInCheck, history, dispatch, showComplete]);
 
-  return <div className="print-text">Printing in progress: {printStatus}</div>;
+  return (
+    <div className="flex-container">
+      {!showComplete ? (
+        <>
+          <div className="print-text">Printing in progress...</div>
+          <ProgressBar completed={printStatus > 100 ? 100 : printStatus} />
+        </>
+      ) : (
+        <>
+          <div className="print-text">Printing Complete!</div>
+          <div className="print-text">Pick up photos</div>
+          <div className="finger-point animate-left">&#9758;</div>
+        </>
+      )}
+    </div>
+  );
 };
 export default PrintPage;
